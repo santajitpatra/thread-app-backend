765bacd672f8270021eec0eb2596f2bd7a080529
Santajit Patra <business.santajitpatra@gmail.com>
2023-10-05 00:49:11 +0530

    feat : graphql setup
    graphql setup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,43 @@
-import express from 'express';
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
 
-const app = express()
-const port = Number(process.env.PORT) ||8000
+async function init() {
+  const app = express();
+  const port = Number(process.env.PORT) || 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  // create graphql server
+
+  const server = new ApolloServer({
+    // Schema
+    typeDefs: `type Query{
+      hello: String
+      say(name:String): String
+    }`,
+    // function
+    resolvers: {
+      Query: {
+        hello: () => `Hello there!`,
+        say: (_, {name}:{name: string}) => `Hey ${name}, How are you!`
+      },
+    },
+  });
+
+  // start graphql server
+
+  await server.start();
+
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+  app.use("/graphql", expressMiddleware(server));
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+
+init();
